8e2181d907f8150e63989115ce45b715f957e00f
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2026-06-01 11:54:25 +0300

    add login test for sonarqube

---
+++ playwright/tests/login_test.spec.js
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+    const targetURL = process.env.BASE_URL;
+    const username = process.env.TEST_USERNAME;
+    const password = process.env.TEST_PASSWORD;
+
+    // fail the test if missing env vars
+    if (!targetURL || !username || !password) {
+        throw new Error('Missing environment variables!');
+    }
+
+    await page.goto(targetURL);
+
+    // pause the test when run in '--headed' mode to see
+    // what it does step by step
+    // await page.pause();
+
+    // wrap only the typing actions in a generic step that doesn't show
+    // credentials in plain text in the test report
+    await test.step('Entering credentials and submitting form', async () => {
+        await page.getByRole('textbox', { name: 'Username' }).fill(username);
+        await page.getByRole('textbox', { name: 'Password' }).fill(password);
+        await page.getByRole('button', { name: 'Log in' }).click();
+    })
+
+    await expect(page).toHaveTitle('Projects - SonarQube Community Build');
+
+    await page.getByRole('link', { name: 'wallet-api' }).first().click();
+    await expect(page.getByRole('heading', { name: 'Overview' })).toBeVisible();
+});
