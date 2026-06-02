ab32031acf93c2427b7221794dfe697bae5d65cd
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2026-06-02 15:10:40 +0300

    test post and delete endpoints with admin key

--- playwright/tests/api/compliments_api.spec.js
+++ playwright/tests/api/compliments_api.spec.js
@@ -3,6 +3,8 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Compliments API Test', async () => {
 
+    const admin_key = process.env.ADMIN_KEY;
+
     test('Get a Random compliment', async ({ request }) => {
 
         // Returns a random compliment.
@@ -12,11 +14,11 @@ test.describe('Compliments API Test', async () => {
         }).then((result) => {
             console.log(result.text)
         })
-        .catch((error) => {
-            console.error('error, try again')
-            // to get information about the error
-            console.error(error)
-        })
+            .catch((error) => {
+                console.error('error, try again')
+                // to get information about the error
+                console.error(error)
+            })
 
     })
 
@@ -42,6 +44,47 @@ test.describe('Compliments API Test', async () => {
 
         console.log(result.text)
         expect(result.text).toContain('smile')
+
+    })
+
+    // Test Post endpoint
+    // Put endpoint is similar to Post
+    // (change from request.post to request.put)
+    // and expect a 200 status code instead of 201
+    // since put just modifies data, not create new data.
+    test('Post a Compliment', async ({ request }) => {
+
+        const response = await request.post('https://api.ibrahimmoalim.dev/compliments', {
+            // input admin key if required
+            headers: {
+                "x-api-key": admin_key
+            },
+            data: {
+                "text": "I bet you are the favorite part of someone's day right now."
+            }
+        })
+
+        const result = await response.json()
+        console.log(result.text)
+
+        // expect a 201 (request created new resource)
+        expect(response.status()).toBe(201);
+        expect(response.ok()).toBeTruthy();
+        expect(result.text).toContain('favorite');
+
+    })
+
+    // Test Delete endpoint (delete a specific compliment by ID)
+    test('Delete a Compliment by ID', async ({ request }) => {
+
+        const response = await request.delete('https://api.ibrahimmoalim.dev/compliments/6a1ebd95a26ee5649f4858d5', {
+            headers: {
+                "x-api-key": admin_key
+            }
+        })
+
+        expect(response.status()).toBe(200);
+        expect(response.ok()).toBeTruthy();
 
     })
 
